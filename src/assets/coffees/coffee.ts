e0c7b59bdb4838a29cb0fe269ddcf12e4662d8a1
Salomao S <souza95salomao@gmail.com>
2022-07-17 19:09:31 +0000

    update: adds mocked coffee list

---
+++ src/assets/coffees/coffee.ts
@@ -0,0 +1,33 @@
+import Americano from './americano.png'
+import Arabe from './arabe.png'
+import AuLatte from './coffee-au-latte.png'
+import Capuccino from './capuccino.png'
+import Cubano from './cubano.png'
+import Expresso from './expresso.png'
+import ExpressoCream from './expresso-cream.png'
+import Havaiano from './havaiano.png'
+import HotChocolatte from './hot-chocolatte.png'
+import IcedCoffee from './iced-coffee.png'
+import Ireland from './irlandes.png'
+import Latte from './latte.png'
+import Macchiatto from './macchiato.png'
+import Mochaccino from './mochaccino.png'
+
+export const coffees = [
+	{name:'American', price:'10,90', image:Americano, id:'american-38d8'},
+	{name:'Arabian', price:'35,56', image:Arabe, id:'arabian-dh3i'},
+	{name:'Au Latte', price:'18,90', image:AuLatte, id:'au-latte-ir8g'},
+	{name:'Capuccino', price:'8,90', image:Capuccino, id:'capuccino-918j'},
+	{name:'Cuban', price:'7,56', image:Cubano, id:'cuban-e77d'},
+	{name:'Expresso', price:'9,99', image:Expresso, id:'expresso-8e7h'},
+	{name:'Expresso Cream', price:'12,67', image:ExpressoCream, id:'expresso-cream-2f8c'},
+	{name:'Havaian', price:'12,65', image:Havaiano, id:'havaian-2r9l'},
+	{name:'Hot Chocolatte', price:'3,45', image:HotChocolatte, id:'hpt-choco-me7j'},
+	{name:'Iced Coffer', price:'7,50', image:IcedCoffee, id:'iced-coffee-o28d'},
+	{name:'Ireland', price:'14,60', image:Ireland, id:'ireland-39rj'},
+	{name:'Latte', price:'8,99', image:Latte, id:'latte-o28s'},
+	{name:'Macchiatto', price:'16,75', image:Macchiatto, id:'macchiatto-3r99'},
+	{name:'Mochaccino', price:'11,89', image:Mochaccino, id:'mochaccino-928r'}
+]
+
+export type Coffee = typeof coffees[0]
