e1d41d47ceaca5ea2fb6c9b0c8cbaf18795e052a
Salomao S <souza95salomao@gmail.com>
2022-07-22 17:00:11 +0000

    chore: adds input component

---
+++ src/components/input.tsx
@@ -0,0 +1,48 @@
+import { ChangeEvent, HTMAttributes, useState } from 'react'
+
+import {
+  useTheme,
+  InputGroup,
+  InputRightElement,
+  Input as ChakraInput,
+} from '@chakra-ui/react'
+
+type InputProps = HTMLAttributes<HTMLInputElement> & {
+  optional?: boolean
+}
+
+export function Input({ optional = false, ...props }: InputProps) {
+  const theme = useTheme()
+  const [value, setValue] = useState('')
+
+  function handleChange(e: ChangeEvent) {
+    setValue(e.target.value)
+  }
+
+  const showOptionalText = optional && !!!value
+
+  return (
+    <InputGroup>
+      {showOptionalText && (
+        <InputRightElement
+          pointerEvents="none"
+          color={theme.colors.base.label}
+          fontSize="1rem"
+          mr={8}
+          children="Optional"
+        />
+      )}
+
+      <ChakraInput
+        bg={theme.colors.base.input}
+        color={theme.colors.base.text}
+        onChange={handleChange}
+        _placeholder={{
+          color: theme.colors.base.label,
+        }}
+        focusBorderColor={theme.colors.yellow.medium}
+        {...props}
+      />
+    </InputGroup>
+  )
+}
